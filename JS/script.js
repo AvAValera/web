ad8e9663abe6caac8b349cb9ecc399a45cffe2b4
AvAValera <avavarenik07@gmail.com>
2020-12-10 16:04:52 +0200

    edit JS

--- JS/script.js
+++ JS/script.js
@@ -10,6 +10,35 @@ cat = {
   age: 7,
   color: 'grey'
 } ];
+console.log(array[1].breed, array[0].breed);
 
-console.log(array);
+let shop = [
+  ["apple", 10, true],
+  ["onion", 3, true],
+  ["patates", 5, false]
+];
+shop[3] = ["carrot", 12, true]
+console.log(shop[0][0], shop[3][0]);
 
+let colorArrary = ['white', 'black', 'blue', 'yellow', 'red', 'pink', 'green'];
+for(i in colorArrary){
+  document.write(colorArrary[i]);
+}
+for(let result of colorArrary)
+  document.write(result + " ");
+
+function display(){
+  document.write('<br>New function')
+}
+display();
+function sum(a, b, c){
+  document.write('<br>', a + b + c);
+}
+sum(1,2,3)
+function arg(){
+  for(i = 0; i < arguments.length; i++){
+    console.log(arguments[i]+100);
+  }
+}
+arg();
+$('.qqq').css('color', 'red');
