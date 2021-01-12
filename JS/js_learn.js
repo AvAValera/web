ef060eefb342589132f041ca2f3c42b0e5fa5566
AvAValera <avavarenik07@gmail.com>
2021-01-12 15:09:23 +0200

    add learn project JS

---
+++ JS/js_learn.js
@@ -0,0 +1,8 @@
+// variable
+var firstName = 'Well';
+const lastName = 'Wells';
+let age = 23;
+const human = true;
+
+console.log('My name is: ' + firstName + ' I\'m years old: ' + age);
+console.log(typeof firstName, typeof human, typeof age);
