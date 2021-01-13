20ea4916506f72404a356c793c89b920da45ab1f
AvAValera <avavarenik07@gmail.com>
2021-01-13 22:44:08 +0200

    edit Js learn

--- JS/js_learn.js
+++ JS/js_learn.js
@@ -6,3 +6,33 @@ const human = true;
 
 console.log('My name is: ' + firstName + ' I\'m years old: ' + age);
 console.log(typeof firstName, typeof human, typeof age);
+
+let names = ['John', 'Johns', 'Mike', 'Sam'];
+let rand = Math.floor(Math.random() * names.length)
+console.log(names[(Math.floor(Math.random() * names.length))])
+console.log(Math.floor(Math.random()*(5-2+1))+2)
+
+//Strings
+let myName = 'Tom';
+let myAge = 33;
+
+const output = 'Hi, my name '+myName+' and '+myAge+' years old';
+var out = `Hi my name is ${myName} and ${myAge} years old to me` ;
+console.log(out);
+
+let testName = 'Mike'
+console.log(testName.toUpperCase());
+console.log(testName.toLowerCase());
+console.log(testName.charAt(2));
+console.log(testName.indexOf('ke'));
+console.log(testName.repeat(5));
+
+
+
+
+
+
+
+
+
+
