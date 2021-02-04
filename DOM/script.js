04c969f94d0c7385fbe0032e78a3851fc54f814f
AvAValera <avavarenik07@gmail.com>
2021-02-04 16:37:40 +0200

    edit DOM

--- DOM/script.js
+++ DOM/script.js
@@ -1,6 +1,17 @@
-const cont = document.querySelector('.content');
+const content = document.querySelector('.content');
+    btn = document.querySelector('.btn');
+    inp = document.querySelector('.inp');
+    message = document.querySelector('.message');
+    btnClear = document.querySelector('.btnClear');
 
-function clickButton(){
-    console.log(cont);
+btn.addEventListener('click',() =>{
+    message.style.border = '1px solid black';
+    let inputEnter = inp.value;
+    message.innerHTML += `${inputEnter} <br>`
     
-};
+    inp.value = '';
+})
+btnClear.addEventListener('click',() =>{
+    message.innerHTML = '';
+    message.style.border = 'none';
+});
