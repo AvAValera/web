03ab5d52b73f442f3f52b0b559e4d649234c9628
AvAValera <avavarenik07@gmail.com>
2021-02-04 21:08:36 +0200

    edit DOM

--- DOM/script.js
+++ DOM/script.js
@@ -3,15 +3,71 @@ const content = document.querySelector('.content');
     inp = document.querySelector('.inp');
     message = document.querySelector('.message');
     btnClear = document.querySelector('.btnClear');
-
+    en = document.querySelectorAll('.en');
+    ru = document.querySelectorAll('.ru');
+    btnTranslate = document.querySelector('.btn_translate');
+    btnColor = document.querySelector('.btn_color')
+    page = document.querySelector('.page')
 btn.addEventListener('click',() =>{
-    message.style.border = '1px solid black';
-    let inputEnter = inp.value;
-    message.innerHTML += `${inputEnter} <br>`
+    if(inp.value.trim().length > 0){
+        message.style.border = '1px solid black';
+        message.innerHTML += `${inp.value} <br>`
+    }
     
     inp.value = '';
 })
-btnClear.addEventListener('click',() =>{
-    message.innerHTML = '';
+btnClear.addEventListener('click',(e) =>{
+    message.textContent = '';
     message.style.border = 'none';
+    document.write = e.target
+    
+});
+
+for (let i = 0; i < ru.length; i++){
+    ru[i].style.display = 'none';
+
+}
+
+function checkRU(arg){
+    for (let i = 0; i < ru.length; i++){
+        ru[i].style.display = `${arg}`;
+}};
+function checkEN(arg){
+    for (let i = 0; i < en.length; i++){
+        en[i].style.display = `${arg}`;
+}};
+
+let checkBtn = true;
+btnTranslate.addEventListener('click',() =>{
+    if(checkBtn){
+        checkRU('inline');
+        checkEN('none');
+        btnTranslate.textContent = 'RU'
+        checkBtn = false;
+        }
+    else{
+        checkRU('none');
+        checkEN('inline');
+        btnTranslate.textContent = 'EN'
+        checkBtn = true;
+        
+    }
+    
+});
+
+btnColor.addEventListener('click', (e) =>{
+    page.style.transition = '0s';
+    page.style.color = 'white';
+    page.style.backgroundColor = 'grey';
+    setTimeout(() => {
+        page.style.transition = '1s';
+    },1000)
+
+    document.querySelector('body').style.backgroundColor = 'black';
+});
+page.addEventListener('mouseover', (e) =>{
+    page.style.borderRadius = '50px';
+});
+page.addEventListener('mouseout', (e) =>{
+    page.style.borderRadius = '10px';
 });
