cf7be3b8c0acb840bf35f99ef3f6dfc93ae3408c
AvAValera <avavarenik07@gmail.com>
2021-02-06 14:26:09 +0200

    edit todo project

--- DOM/TODO/script.js
+++ DOM/TODO/script.js
@@ -1,29 +1,53 @@
 const elements = document.querySelector('.todo_elements');
-const btnClear = document.querySelector('.btn_clear');
-const btnEnter = document.querySelector('.btn_enter')
+        btnClear = document.querySelector('.btn_clear');
+        btnEnter = document.querySelector('.btn_enter')
+        error = document.querySelector('.error');
+        inp = document.querySelector('.input');
 
+        const check = document.createElement('input')
+        check.setAttribute('type', 'checkbox');
 
 btnClear.addEventListener('click', () =>{
         elements.innerHTML = '';
+        inp.value = '';
+        numbElem = 0;
     });
 
-btnEnter.addEventListener('click', () =>{
-    const inp = document.querySelector('.input');
+
+function addElementToList(){
     if(inp.value.trim().length > 2 && inp.value.trim().length < 24){
-        const check = document.createElement('input')
-        check.type = 'checkbox';
+        error.textContent = '';
+        var check = document.createElement('input')
+        check.setAttribute('type', 'checkbox');
+        
+        check.classList.add(`inputCheck`);
         const newElem = document.createElement('div');
+        newElem.className = 'elem_block';
         const paragraph = document.createElement('p');
         elements.appendChild(newElem);
         newElem.appendChild(paragraph);
-        paragraph.classList.add('elem_paragraph')
+        paragraph.classList.add(`elem_paragraph`);
         newElem.appendChild(check);
         paragraph.textContent = inp.value;
-        inp.value = '';
-        
     }
-    if(inp.value.trim().length <= 2){
-        const error = document.querySelector('.error');
+    if(inp.value.length <= 2 || inp.value.length >= 25){
         error.textContent = 'Error!';
     }
+    
+    inp.value = '';
+};
+
+btnEnter.addEventListener('click', () =>{
+    addElementToList();
 });
+
+inp.addEventListener('keydown',(event) => {
+    if(event.keyCode === 13){
+        addElementToList();
+    };
+});
+
+
+
+
+
