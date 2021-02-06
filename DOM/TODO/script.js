131529e61e7619ac1fd58197d391aa62fd073a56
AvAValera <avavarenik07@gmail.com>
2021-02-06 10:44:43 +0200

    create error block in to do list

--- DOM/TODO/script.js
+++ DOM/TODO/script.js
@@ -9,14 +9,21 @@ btnClear.addEventListener('click', () =>{
 
 btnEnter.addEventListener('click', () =>{
     const inp = document.querySelector('.input');
-    if(inp.value.trim().length > 2){
+    if(inp.value.trim().length > 2 && inp.value.trim().length < 24){
         const check = document.createElement('input')
         check.type = 'checkbox';
         const newElem = document.createElement('div');
+        const paragraph = document.createElement('p');
         elements.appendChild(newElem);
-        
-        newElem.textContent = inp.value;
-        inp.value = '';
+        newElem.appendChild(paragraph);
+        paragraph.classList.add('elem_paragraph')
         newElem.appendChild(check);
+        paragraph.textContent = inp.value;
+        inp.value = '';
+        
+    }
+    if(inp.value.trim().length <= 2){
+        const error = document.querySelector('.error');
+        error.textContent = 'Error!';
     }
 });
